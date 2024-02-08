4e57c5b30b2e199c2d4d34b78289345130bb4d6e
gilmacanoe <gilmarpro97@hotmail.com>
2024-02-08 11:40:59 +0000

    at

---
+++ 006/ambiente2.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log (`Passos ${c}`)
+    c++
+}
+*/ 
+//----------
+console.log('VAI COMECAR!')
+for( var c = 1;c <= 3;c++) {
+    console.log(c)
+}
+console.log('FIM!')
